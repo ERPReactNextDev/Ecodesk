ed141cb8dce3f601c20e670d1956db2d639df091
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-08-13 16:30:54 +0800

    Leroux Y Xchire

--- app/ModuleCSR/CSR/WebsiteInquiry/page.tsx
+++ app/ModuleCSR/CSR/WebsiteInquiry/page.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import ParentLayout from "../../components/Layouts/ParentLayout";
 import SessionChecker from "../../components/Session/SessionChecker";
 import UserFetcher from "../../components/User/UserFetcher";
-import AddAccountForm from "../../components/Accounts/AddAccountForm";
+import AddAccountForm from "../../components/Accounts/Form";
 import SearchFilters from "../../components/WebsiteInquiry/SearchFilters";
 import Inquirytable from "../../components/WebsiteInquiry/InquiryTable";
 import Pagination from "../../components/Accounts/Pagination";
